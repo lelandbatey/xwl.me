fce270cecd5207bc3654af20dd3a002fb674ba0f
Leland Batey <lelandbatey@gmail.com>
2016-10-29 09:12:11 -0700

    Add support for environment variable configuration

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -8,12 +8,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+import os
+
 
 def make_engine(path="src_url_database.sqlite3"):
     """Create an SqlAlchemy engine with the given path."""
     return create_engine('sqlite:///'+path)
 
-engine = make_engine()
+if 'XWL_DB_PATH' in os.environ:
+    engine = make_engine(os.environ['XWL_DB_PATH'])
+else:
+    engine = make_engine()
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
--- run.py
+++ run.py
@@ -4,8 +4,15 @@
 from __future__ import print_function
 from app import frontend
 
+import os
+
+if 'XWL_ROOT_URL' in os.environ:
+	ROOT_URL = os.environ['XWL_ROOT_URL']
+else:
+	ROOT_URL = "http://localhost:5000/"
+
 APP = frontend.APP
-APP.config.update({"ROOT_URL":"http://localhost:5000/"})
+APP.config.update({"ROOT_URL":ROOT_URL})
 
 if __name__ == '__main__':
-	APP.run(debug=True, host="0.0.0.0")
+	APP.run(host="0.0.0.0")
